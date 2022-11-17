40b114283b7cc5f6c91f1e710625e1c67976a3a8
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-11-17 08:27:51 +0530

    Update sendMessage.js

--- sendMessage.js
+++ sendMessage.js
@@ -2,8 +2,8 @@ const axios = require("axios").default;
 
 module.exports = async (chat_id, text) => {
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    chat_id,
-    text,
+  chat_id: JSON.parse(event.body).message.chat.id,
+  text: "eew pakaya!",
   });
 
   return true;
